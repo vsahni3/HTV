619344d24307715be54de5e966620c2e81e2e782
michaelZhaoCode <82855036+michaelZhaoCode@users.noreply.github.com>
2022-10-15 17:43:56 -0400

    added flask functionality - implemented working login form - implemented sessions - added login required check - added index homepage to use sessions demonstration

--- app/main.py
+++ app/main.py
@@ -1,22 +1,58 @@
-from django.shortcuts import render
-from flask import Flask, render_template, request
-app = Flask(__name__)
+from flask import Flask, render_template, request, redirect, session
+from flask_session import Session
+from functools import wraps
+from tempfile import mkdtemp
 
-@app.route("/")
+app = Flask(__name__)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("username") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template('login.html')
+    """Log user in"""
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        session.clear()
+
+        username = request.form.get("username")
+        password = request.form.get("password")
+        print(username, password)
+        session["username"] = username
+        session["password"] = password
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
 
 # @app.route("/login")
 # def user():
 #     return request.url
 
-@app.route("/pwd", methods=['POST', 'GET'])
-def pwd():
-    if request.method == "POST":
-        if "form1" in request.form:
-            return "form1"
-        elif "form2" in request.form:
-            return "form2"
+@app.route("/", methods=['GET'])
+@login_required
+def index():
+    if request.method == 'GET':
+        return render_template('index.html', user=session["username"], password=session["password"])
 
 @app.route("/upload-file", methods=["POST", "GET"])
 def uploader():
@@ -24,9 +60,10 @@ def uploader():
         f = request.files['file']
         f.save(f.filename)
         return f.filename
-        
-        
+
+
 @app.route("/upload")
+@login_required
 def upload():
     return render_template('upload.html')
 
